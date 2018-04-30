30a7f341ea69f72ae4a9c5c1b70d2b2b5e820e17
sovos <sovos15@gmail.com>
2018-04-29 18:51:23 -0700

    V1.1 Commit
    So much better

--- Creator News v1.1.rb
+++ Creator News v1.1.rb
@@ -1,8 +1,10 @@
 require 'open-uri'
 
 url = 'https://newsapi.org/v2/everything?'\
-      'q=Youtuber&'\
-      'sortBy=popularity&'\
+      'Q=content creators&'\
+      'sortBy=relevancy&'\
+      'from=d.month_before.strftime("%d/%m/%Y")&'\
+      'to=d.strftime("%d/%m/%Y")&'\
       'apiKey=cc4fca55933b41caafd2f58dfe8d882e'
 
 req = open(url)
@@ -11,6 +13,6 @@ response_body = req.read
 
 puts response_body
 
-fh = File.open('newsapi.json', 'w')
+fh = File.open('Creator News.json', 'w')
 fh.puts response_body
 fh.close
---
+++ Hosting News v1.1.rb
@@ -0,0 +1,18 @@
+require 'open-uri'
+
+url = 'https://newsapi.org/v2/everything?'\
+      'Q=video hosting&'\
+      'sortBy=relevancy&'\
+      'from=d.month_before.strftime("%d/%m/%Y")&'\
+      'to=d.strftime("%d/%m/%Y")&'\
+      'apiKey=cc4fca55933b41caafd2f58dfe8d882e'
+
+req = open(url)
+
+response_body = req.read
+
+puts response_body
+
+fh = File.open('hosting News.json', 'w')
+fh.puts response_body
+fh.close
